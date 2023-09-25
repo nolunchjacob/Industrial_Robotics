e91aa31c1a7814f2086f54419538eced2da706d4
calvi <calvi@DESKTOP-J1UDUD7.modem>
2023-09-25 13:36:34 +1000

    Adding WidowX200 file - Sam 25/09/23

--- Assignment2.m
+++ Assignment2.m
@@ -6,3 +6,8 @@
     % Below are specifications for the WidowX - 200 
     % https://docs.trossenrobotics.com/interbotix_xsarms_docs/specifications/wx200.html
   
+ % We need to sort out a method for document control, I think that we
+ % should have different responsibilities and try to keep code seperated 
+ % like create functions in different files and then call them within this file
+ % or something along those line, that way we are not writing over
+ % eachothers work

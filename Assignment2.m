888669f6bae09994534ccd0236d16103eae863a2
calvi <calvi@DESKTOP-J1UDUD7.modem>
2023-09-25 17:50:01 +1000

    Cleaning up the Assignment 2 File - Sam 25/09/23

--- Assignment2.m
+++ Assignment2.m
@@ -3,8 +3,7 @@
 %% Robots being used : 
 % Dobot Magician 
 % WidowX-200 Desktop Arm
-    % Below are specifications for the WidowX - 200 
-    % https://docs.trossenrobotics.com/interbotix_xsarms_docs/specifications/wx200.html
+  
   
  % We need to sort out a method for document control, I think that we
  % should have different responsibilities and try to keep code seperated 

2caa8e74731c88ec8649e540f806d9e53624f3c3
calvi <calvi@DESKTOP-AMHBBMT.modem>
2023-10-09 01:51:55 -0700

    WidowX200-Model-STL files, cannot seem to be able to push a folder sorry - Sam 9/10/2023

---
+++ WidowX200_STL_FILES/README.m
@@ -0,0 +1,5 @@
+% The source of the STL Files are belo,
+%https://github.com/Interbotix/interbotix_ros_manipulators/tree/main/interbotix_ros_xsarms/interbotix_xsarm_descriptions/meshes/wx200_meshes
+
+%% The source was found on the website below 
+%% https://docs.trossenrobotics.com/interbotix_xsarms_docs/specifications/wx200.html

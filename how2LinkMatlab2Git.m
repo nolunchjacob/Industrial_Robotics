694d439a1ab09b5de5f3d48c1224b3330053bd7c
calvi <calvi@DESKTOP-AMHBBMT.modem>
2023-10-09 01:36:05 -0700

    Changes to web link of instructional guide to linking matlab w/ Git - Sam 9/10/23

--- how2LinkMatlab2Git.m
+++ how2LinkMatlab2Git.m
@@ -3,9 +3,9 @@
 
 %% OK group I have successfully pushed this from Matlab to Git, It was actually pretty easy 
 %% Just follow the link below 
-%% https://au.mathworks.com/help/matlab/matlab_prog/use-git-in-matlab.html
+%% https://osulp.github.io/git-advanced/06_integrate_matlab_and_git/index.html
 
-%% I also followed this link and installed the "Command line Git Client"
+%% I also followed this link and installed the "Command line Git Client" IMPORTANT
 %% https://au.mathworks.com/help/matlab/matlab_prog/set-up-git-source-control.html#mw_b5884d8b-7c3e-4c38-b055-28e39b604147" 
 
 

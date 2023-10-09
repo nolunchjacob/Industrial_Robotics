eadb898dd86a024f8c7883d70a981b586ef25872
calvi <calvi@DESKTOP-AMHBBMT.modem>
2023-10-09 10:10:08 -0700

    Saving progress

--- WidowX200.m
+++ WidowX200.m
@@ -2,27 +2,43 @@ classdef WidowX200 < RobotBaseClass
 
 
     properties(Access = public)
-        plyFileNameStem = "WidowX200"
+        plyFileNameStem = 'WidowX200';
     end
 
     % Constructor
-    methods(Access = public)
-        function self = WidowX200(baseTr)
-            self.CreateModel();
-            if nargin == 1
-                self.model.base = self.model.base.T * baseTr;
+    %methods(Access = public)   
+    methods
+        function self = WidowX200(baseTr,useTool,toolFilename)
+            if nargin <2
+                 baseTr = transl(0,0,0);  
             end
-            self.PlotAndColourRobot();          
+        else 
+                self.useTool = useTool;
+                toolTrData = load([toolFilename,'.mat']);
+                self.toolTr = toolTrData.tool;
+                self.toolFilename = [toolFilename,'.ply'];
+
+        end    
+            self.model.base = self.model.base.T * baseTr;
+            self.CreateModel();
+            self.PlotAndColourRobot();   
+           % drawnow
         end
 
     %CreateModel
+    %% This is most likely not 100% accurate, I am just trying to model it to see
         function CreateModel(self)
-            link(1) = Link('alpha',-pi/2,'a',0,'d',113.25,'offset',,'qlim',[deg2rad(-180),deg2rad(180)]);
-            link(2) = Link('alpha',,'a',,'d',,'offset',,'qlim',[deg2rad(-108),deg2rad(113)]);
-            link(3) = Link('alpha',,'a',,'d',,'offset',,'qlim',[deg2rad(-108),deg2rad(93)]);
-            Link(4) = Link('alpha',,'a',,'d',,'offset',,'qlim',[deg2rad(-100),deg2rad(123)]);
-            Link(5) = Link('alpha',,'a',,'d',,'offset',,'qlim',[deg2rad(-180),deg2rad(180)]);
-            self.model = SerialLink(link,'name',self.name)
+               %%TOOK AGES but gave me an error if the first link was
+               %%link(0) instead of link(1)
+            %
+           % link(0) = Link('d',0.11325,'a',0,'alpha',pi/2,'offset',0,'qlim',[deg2rad(-180),deg2rad(180)]);
+            link(1) = Link('d',0.11325,'a',0,'alpha',1.570795,'offset',0,'qlim',deg2rad([-180 180]));
+            link(2) = Link('d',0,'a',0.20616,'alpha',0,'offset',0,'qlim',deg2rad([-108 113]));
+            link(3) = Link('d',0,'a',0.200,'alpha',0,'offset',0,'qlim',deg2rad([-108 93]));
+            link(4) = Link('d',0,'a',0.065,'alpha',0,'offset',0,'qlim',deg2rad([-100 123]));
+            link(5) = Link('d',0.10916,'a',0,'alpha',pi/2,'offset',0,'qlim',deg2rad([-180 180]));
+
+            self.model = SerialLink(link,'name',self.name);
         end
     end
 end

7adf12038de5aeb59bc866c6e68e9fb540e24095
calvi <calvi@DESKTOP-AMHBBMT.modem>
2023-10-09 21:44:42 -0700

    Test Ply Files - Sam 9/10/23

---
+++ TestingObjects.m
@@ -0,0 +1,11 @@
+%WidowX200Link1.ply good
+%WidowX200Link2.ply good 
+%WidowX200Link3.ply good
+%WidowX200Link4.ply good 
+%WidowX200Link5.ply good
+%WidowX200Link6.ply good
+
+
+PlaceObject('WidowX200Link7.ply')
+axis equal
+camlight

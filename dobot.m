8e1d435fadd15b9805ce1feaf62c7e8dfb596211
jheap20 <jackson.t.heap@student.uts.edu.au>
2023-10-23 12:06:50 +1100

    Not sure why the forward kinematics is slightly off

--- dobot.m
+++ dobot.m
@@ -3,23 +3,30 @@ clc
 
 
 hold on;
+% to add environment uncomment line below and uncomment the axis
 % visualize_and_analyze_robot_test;
 % axis([-2, 3, -2, 2, 0, 2]); % Set axis limits to match your robot's workspace
-% baseTr = transl([-0.5, 0, 0.5]) * trotx(0) * troty(0) * trotz(0);
-baseTr = transl([-0.5, 0, 0]) * trotx(0) * troty(0) * trotz(0);
+
+
+baseTr = transl([-0.5, 0, 0.5]) * trotx(0) * troty(0) * trotz(0);
+
+
+% Initialise robot
     robot = DobotMagician(baseTr);
-    q = robot.model.getpos();
-    robot.model.fkine(q);
-% Define position vectors for q1 to q9 (adjust the values as needed)
-    pos_coin1 = [-0.3, -0.2, 0];
-    pos_coin2 = [-0.4, -0.2, 0];
-    pos_coin3 = [-0.5, -0.2, 0];
-    pos_coin4 = [-0.3, 0.2, 0];
-    pos_coin5 = [-0.4, 0.2, 0];
-    pos_coin6 = [-0.5, 0.2, 0];
+    % q = robot.model.getpos();
+    % robot.model.fkine(q);
 
 
-    % % Call brick_pos function with each position vector to place bricks
+% set different coin positions
+    pos_coin1 = [-0.3, -0.2, 0.5];
+    pos_coin2 = [-0.4, -0.2, 0.5];
+    pos_coin3 = [-0.5, -0.2, 0.5];
+    pos_coin4 = [-0.3, 0.2, 0.5];
+    pos_coin5 = [-0.4, 0.2, 0.5];
+    pos_coin6 = [-0.5, 0.2, 0.5];
+
+
+% call the coin models at the coin positions
     fivecentcoin_pos(pos_coin1);
     fivecentcoin_pos(pos_coin2);
     fivecentcoin_pos(pos_coin3);
@@ -29,8 +36,12 @@ baseTr = transl([-0.5, 0, 0]) * trotx(0) * troty(0) * trotz(0);
 
 % keyboard;
 
-% Define a trajectory of joint configurations
-q_startpos = robot.model.ikcon(transl(-0.2, 0, 0.1));
+
+% use ikcon to find desired joint angles for different coin pos
+% coin starting pos
+q_startpos = robot.model.ikcon(transl(-0.2, 0, 0.6));
+
+% rotate 180 degrees about x axis so claw faces down
 q1 = robot.model.ikcon(transl(pos_coin1) * trotx(pi));
 q2 = robot.model.ikcon(transl(pos_coin2) * trotx(pi));
 q3 = robot.model.ikcon(transl(pos_coin3) * trotx(pi)); 
@@ -39,10 +50,11 @@ q5 = robot.model.ikcon(transl(pos_coin5) * trotx(pi));
 q6 = robot.model.ikcon(transl(pos_coin6) * trotx(pi));
 
 
-%drop off positions
-q_drop = robot.model.ikcon(transl(-0.3, 0, 0.1) * trotx(pi));
+%drop off position
+q_drop = robot.model.ikcon(transl(-0.3, 0, 0.65) * trotx(pi));
 
 
+% robot moves from starting pos->1st coin-> (drop off->next coin (repeat))
 trajectory1 = jtraj(q_startpos, q1, 50);
 trajdrop1 = jtraj(q1,q_drop, 50);
 
@@ -65,13 +77,15 @@ trajdrop6 = jtraj(q6, q_drop, 50);
 
 
 % Animate the robot's motion along the trajectory
+
+% starting pos->1st coin->drop off
 for i = 1:size(trajectory1, 1)
     robot.model.animate(trajectory1(i, :));
     drawnow;
     pause(0.1);
     
 end
-disp(['Forward Kinematics:'])
+disp(['Pick up 1 Forward Kinematics:'])
 disp(robot.model.fkine(robot.model.getpos()));
 
 for i = 1:size(trajdrop1, 1)
@@ -80,14 +94,17 @@ for i = 1:size(trajdrop1, 1)
     pause(0.1);
     
 end
+disp(['Drop off Forward Kinematics:'])
+disp(robot.model.fkine(robot.model.getpos()));
 
 
+%drop off->coin 2->drop off
 for i = 1:size(trajectory2, 1)
     robot.model.animate(trajectory2(i, :));
     drawnow;
     pause(0.1);
 end
-disp(['Forward Kinematics:'])
+disp(['Pick up 2 Forward Kinematics:'])
 disp(robot.model.fkine(robot.model.getpos()));
 
 for i = 1:size(trajdrop2, 1)
@@ -96,17 +113,17 @@ for i = 1:size(trajdrop2, 1)
     pause(0.1);
     
 end
-disp(['Forward Kinematics:'])
+disp(['Drop off Forward Kinematics:'])
 disp(robot.model.fkine(robot.model.getpos()));
 
 
-
+%drop off->coin 3->drop off
 for i = 1:size(trajectory3, 1)
     robot.model.animate(trajectory3(i, :));
     drawnow;
     pause(0.1);
 end
-disp(['Forward Kinematics:'])
+disp(['Pick up 3 Forward Kinematics:'])
 disp(robot.model.fkine(robot.model.getpos()));
 
 for i = 1:size(trajdrop3, 1)
@@ -115,18 +132,17 @@ for i = 1:size(trajdrop3, 1)
     pause(0.1);
     
 end
-disp(['Forward Kinematics:'])
+disp(['Drop off Forward Kinematics:'])
 disp(robot.model.fkine(robot.model.getpos()));
 
 
-
-
+%drop off->coin 4->drop off
 for i = 1:size(trajectory4, 1)
     robot.model.animate(trajectory4(i, :));
     drawnow;
     pause(0.1);
 end
-disp(['Forward Kinematics:'])
+disp(['Pick up 4 Forward Kinematics:'])
 disp(robot.model.fkine(robot.model.getpos()));
 
 
@@ -136,18 +152,17 @@ for i = 1:size(trajdrop4, 1)
     pause(0.1);
     
 end
-disp(['Forward Kinematics:'])
+disp(['Drop off Forward Kinematics:'])
 disp(robot.model.fkine(robot.model.getpos()));
 
 
-
-
+%drop off->coin 5->drop off
 for i = 1:size(trajectory5, 1)
     robot.model.animate(trajectory5(i, :));
     drawnow;
     pause(0.1);
 end
-disp(['Forward Kinematics:'])
+disp(['Pick up 5 Forward Kinematics:'])
 disp(robot.model.fkine(robot.model.getpos()));
 
 for i = 1:size(trajdrop5, 1)
@@ -156,19 +171,17 @@ for i = 1:size(trajdrop5, 1)
     pause(0.1);
     
 end
-disp(['Forward Kinematics:'])
+disp(['Drop off Forward Kinematics:'])
 disp(robot.model.fkine(robot.model.getpos()));
 
 
-
-
-
+%drop off->coin 6->drop off
 for i = 1:size(trajectory6, 1)
     robot.model.animate(trajectory6(i, :));
     drawnow;
     pause(0.1);
 end
-disp(['Forward Kinematics:'])
+disp(['Pick up 6 Forward Kinematics:'])
 disp(robot.model.fkine(robot.model.getpos()));
 
 for i = 1:size(trajdrop6, 1)
@@ -177,6 +190,6 @@ for i = 1:size(trajdrop6, 1)
     pause(0.1);
     
 end
-disp(['Forward Kinematics:'])
+disp(['Drop off Forward Kinematics:'])
 disp(robot.model.fkine(robot.model.getpos()));
 % keyboard;

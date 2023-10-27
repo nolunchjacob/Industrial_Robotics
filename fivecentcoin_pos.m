a52aebc70075c01cf4d87f0ebb2ce79ec2fe26c7
jheap20 <jackson.t.heap@student.uts.edu.au>
2023-10-27 14:10:31 +1100

    Clean up

--- fivecentcoin_pos.m
+++ fivecentcoin_pos.m
@@ -1,6 +1,10 @@
 function coinMesh_h = fivecentcoin_pos(pos_vector)
     % Load the PLY file
-    [f, v] = plyread('HalfSizedRedGreenBrick.ply', 'tri');
+    % [f, v, data] = plyread('fiveCentCoin.ply', 'tri');
+    [f, v, data] = plyread('HalfSizedRedGreenBrick.ply', 'tri');
+
+    % Scale the colors to be 0-to-1 (they are originally 0-to-255)
+    vertexColours = [data.vertex.red, data.vertex.green, data.vertex.blue] / 255;
 
     % Create a 4x4 homogeneous transformation matrix from the provided position vector
     trmatrix = transl(pos_vector); % You can modify the rotation as needed
@@ -12,7 +16,8 @@ function coinMesh_h = fivecentcoin_pos(pos_vector)
     hold on;
 
     % Plot the brick at the specified position
-    coinMesh_h = trisurf(f, v_trmatrix(:, 1), v_trmatrix(:, 2), v_trmatrix(:, 3));
+      coinMesh_h = trisurf(f, v_trmatrix(:, 1), v_trmatrix(:, 2), v_trmatrix(:, 3), ...
+        'FaceVertexCData', vertexColours, 'EdgeColor', 'interp', 'EdgeLighting', 'flat');
     % Add other properties as needed (color, lighting, etc.)
 
     % Configure the view, axis limits, and other settings as needed
@@ -23,28 +28,3 @@ function coinMesh_h = fivecentcoin_pos(pos_vector)
     % keyboard;
 end
 
-% function fivecentcoin_pos(pos_vector)
-%     [f, v, data] = plyread('fiveCentCoin.ply', 'tri');
-% 
-%     % Scale the colors to be 0-to-1 (they are originally 0-to-255)
-%     vertexColours = [data.vertex.red, data.vertex.green, data.vertex.blue] / 255;
-% 
-%     % Create a 4x4 homogeneous transformation matrix from the provided position vector
-%     trmatrix = transl(pos_vector); % You can modify the rotation as needed
-% 
-%     v_trmatrix = (trmatrix(1:3, 1:3) * v' + trmatrix(1:3, 4))';
-% 
-%     % Create a figure for visualization
-%     hold on;
-% 
-%     % Plot the brick at the specified position
-%     coinMesh_h = trisurf(f, v_trmatrix(:, 1), v_trmatrix(:, 2), v_trmatrix(:, 3), ...
-%         'FaceVertexCData', vertexColours, 'EdgeColor', 'interp', 'EdgeLighting', 'flat');
-% 
-%     % Configure the view, axis limits, and other settings as needed
-%     view(3);
-%     grid on;
-% 
-%     % Wait for user input (you can remove this if not needed)
-%     % keyboard;
-% end

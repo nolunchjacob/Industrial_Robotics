0c07b3e6c977fbffa95650cae4648a3dcf4f12f3
calvi@DESKTOP-AMHBBMT.(none) <calvi@DESKTOP-AMHBBMT.modem>
2023-11-02 15:45:49 +1100

    more development on environment and git pull - Sam Carlton 2/11/2023

--- Environment/environmentTest.m
+++ Environment/environmentTest.m
@@ -3,6 +3,8 @@ function environmentTest()
     clc;
 
      % Create a figure for visualization
+
+        
     figure;
     hold on;
     title('3D Model Visualization');
@@ -10,7 +12,20 @@ function environmentTest()
     ylabel('Y');
     zlabel('Z');
     view(3);
-    axis([-2, 3, -2, 2, 0, 2]); % Set axis limits to match your robot's workspace
+    axis([-3, 3, -3, 3, 0, 2]); % Set axis limits to match your robot's workspace
+
+
+    %      scenario = robotScenario(UpdateRate=10);
+    % addMesh(scenario,"Plane",Position=[0 0 0],Size=[4 4],Color=[0.7 0.7 0.7]);
+    % show3D(scenario)
+
+    floor_size = 6;
+floor = patch([-floor_size, floor_size, floor_size, -floor_size], -floor_size, -floor_size, floor_size, floor_size], [0.1, 0.1, 0.1, 0.1]);
+
+
+    
+
+
 
     %% emergency stop button 
    plyfilename1 = 'emergencyStopWallMounted.ply';
@@ -35,48 +50,58 @@ function environmentTest()
     transformationMatrix4 = transl([2.5, 0, 0]) * trotx(0) * troty(0) * trotz(pi);
     modelObject(plyfilename4,transformationMatrix4);
 
-%% barrier
-    plyfilename5 = 'barrier.ply';
-    transformationMatrix5 = transl([1.5, 0, 0]) * trotx(0) * troty(0) * trotz(0);;
-    modelObject(plyfilename5,transformationMatrix5);
+% %% barrier
+%     plyfilename5 = 'barrier.ply';
+%     transformationMatrix5 = transl([1.5, 0, 0]) * trotx(0) * troty(0) * trotz(0);
+%     modelObject(plyfilename5,transformationMatrix5);
 
+% 
+% %% long barrier
+%     plyfilename6 = 'longbarrier.ply';
+%     transformationMatrix6 = transl([-0.3, -1, 0]) * trotx(0) * troty(0) * trotz(-pi/2);
+%     modelObject(plyfilename6,transformationMatrix6);
 
-%% long barrier
-    plyfilename6 = 'longbarrier.ply';
-    transformationMatrix6 = transl([-0.3, -1, 0]) * trotx(0) * troty(0) * trotz(-pi/2);;
-    modelObject(plyfilename6,transformationMatrix6);
+%         %% bookcase
+% plyfilename8 = 'bookcaseTwoShelves0.5x0.2x0.5m.ply';
+% transformationMatrix8 = transl([-0.3, 1.7, 0]) * trotx(0) * troty(0) * trotz(0);
+% modelObject(plyfilename8,transformationMatrix8);
 
-        %% bookcase
-plyfilename8 = 'bookcaseTwoShelves0.5x0.2x0.5m.ply';
-transformationMatrix8 = transl([-0.3, 1.7, 0]) * trotx(0) * troty(0) * trotz(0);
-modelObject(plyfilename8,transformationMatrix8);
-
-            %% chair
-plyfilename9= 'chair.ply';
-transformationMatrix9 = transl([2.5, -1.5, 0]) * trotx(pi/2) * troty(-pi/2) * trotz(0);
-modelObject(plyfilename9,transformationMatrix9);
-
+%             %% chair
+% plyfilename9= 'chair.ply';
+% transformationMatrix9 = transl([2.5, -1.5, 0]) * trotx(pi/2) * troty(-pi/2) * trotz(0);
+% modelObject(plyfilename9,transformationMatrix9);
+% 
 
     %% PERSON
 plyfilename10 = 'personMaleCasual.ply';
 transformationMatrix10 = transl([1, -1.7, 0]) * trotx(0) * troty(0) * trotz((3*pi)/4);
 modelObject(plyfilename10,transformationMatrix10);
 
-% plyfilename11 = ;
-% transformationMatrix11 = ;
-% modelObject(plyfilename11,transformationMatrix11);
-% 
-% plyfilename12 = ;
+plyfilename11 = 'tableNewNew2.ply';
+transformationMatrix11 = transl([2, -1.5, 0]) * trotx(0) * troty(0) * trotz((3*pi)/4);
+modelObject(plyfilename11,transformationMatrix11);
+
+% plyfilename12 = 'five_cent_box_ply.ply';
 % transformationMatrix12 = ;
 % modelObject(plyfilename12,transformationMatrix12);
 % 
-% plyfilename13 = ;
+% plyfilename13 = 'ten_cent_box_ply.ply';
 % transformationMatrix13 = ;
 % modelObject(plyfilename13,transformationMatrix13);
 % 
-% plyfilename14 = ;
+% plyfilename14 = "twenty_cent_coin_box_ply.ply";
 % transformationMatrix14 = ;
 % modelObject(plyfilename14,transformationMatrix14);
+% 
+% plyfilename14 = "one_dollar_coin_box_ply.ply";
+% transformationMatrix14 = ;
+% modelObject(plyfilename14,transformationMatrix14);
+% 
+% plyfilename14 = "two_dollar_coin_box.ply";
+% transformationMatrix14 = ;
+% modelObject(plyfilename14,transformationMatrix14);
+% 
+
 
 
     function modelObject(plyfilename, transformationMatrix)
